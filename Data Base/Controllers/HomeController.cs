6044838714aa614a37b859a79cd9a4a19c7aac21
kinan.karam92 <kinan.karam92@gmail.com>
2018-01-23 16:03:53 +0100

    Add project files.

---
+++ Data Base/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Data_Base.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Data_Base.Controllers
+{
+    public class HomeController : Controller
+    {
+        PeopleDbContext db = new PeopleDbContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Person> myList = db.people.ToList();
+
+            return View(myList);
+        }
+
+        public ActionResult Create()
+        {
+            Person me = new Person();
+            me.Name = "Bobbo";
+            me.Age = 99;
+
+            db.people.Add(me); //Add Bobbo to the Data Base
+            db.SaveChanges();   // Saves The changes (add Bobbo)
+
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Data Base/Models/PeopleDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;   // Add this fore call the data Base
+using System.Linq;
+using System.Web;
+
+namespace Data_Base.Models
+{
+    public class PeopleDbContext : DbContext
+    {
+        public PeopleDbContext() : base("name=PeopleDbContext")
+        {
+
+        }
+
+        public DbSet<Person> people { get; set; }   /// conaction to the list of person Class
+
+    }
+}
